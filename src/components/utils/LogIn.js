be2ddb2c76259b2b6d1abd38b4a7b5aac0f3e297
Sander van Laarhoven <sander.van.laarhoven@oliver-it.nl>
2021-12-07 14:54:36 +0100

    ook bij register doorvoeren

--- src/components/utils/LogIn.js
+++ src/components/utils/LogIn.js
@@ -112,7 +112,7 @@ const LogIn = () => {
 	const register = async () => {
 		if (emailValid && passwordValid && validatePassWordCheck(credentials.password, credentials.passwordCheck)) {
 			const isRIVMEmployee = credentials.email.trim().endsWith('@rivm.nl')
-			const isWhitelisted = credentials.email.trim().endsWith('@Isala.nl') || credentials.email.trim().endsWith('@Etz.nl') || credentials.email.trim().endsWith('@Amsterdamumc.nl') || credentials.email.trim().endsWith('@Ysl.nl')
+			const isWhitelisted = credentials.email.trim().endsWith('@isala.nl') || credentials.email.trim().endsWith('@etz.nl') || credentials.email.trim().endsWith('@amsterdamumc.nl') || credentials.email.trim().endsWith('@ysl.nl')
 			const isSander = credentials.email.trim().endsWith('sandervanlaarhoven@gmail.com')
 			if (!isRIVMEmployee && !isSander && !isWhitelisted) {
 				enqueueSnackbar('Alleen gebruikers van het NHS LIMS mogen deelnemen aan de GAT testen.', {
